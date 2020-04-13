912320176554e3e9fb3c517ac478be1e1e5085e8
AlbertAlquisola <aalquiso@gmail.com>
2020-04-13 12:36:04 -0400

    move port to env var

--- src/app.ts
+++ src/app.ts
@@ -11,4 +11,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 routes(app);
-app.listen(3000, () => console.log('server listening on port 3000'));
+jobs();
+
+app.listen(process.env.PORT, () => console.log(`server listening on port ${process.env.PORT}`));
